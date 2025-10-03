35b692536adc4d90ca1bfeac4a4f0acc5914d105
Nadja Stauber <nadja.stauber@yahoo.de>
2025-10-03 17:19:27 +0200

    markAsDone in List erstellt

--- toDoList/src/app/to-do-list/to-do-list.ts
+++ toDoList/src/app/to-do-list/to-do-list.ts
@@ -9,58 +9,79 @@ import { Task } from '../shared/task';
   templateUrl: './to-do-list.html',
   styleUrl: './to-do-list.css'
 })
-export class ToDoList implements OnInit{    //OnInit --> Ausführung bei Initialisierung der Seite
+export class ToDoList implements OnInit {    //OnInit --> Ausführung bei Initialisierung der Seite
 
   private backendService = inject(Backend) //backend service einbinden, damit Mathoden genutzt werden können
-  allTasks : Task[] = [];     // leeres Task-Array
-  filteredTasks : Task[] = []; 
+  allTasks: Task[] = [];     // leeres Task-Array
+  filteredTasks: Task[] = [];
   deleteStatus: boolean = false;
   task!: Task;
 
   async ngOnInit(): Promise<void> {
     // async Methode, die Promise zurückgibt
     this.allTasks = await this.backendService.getAll(); //Promise Task[] wird zurückgegeben //Array mit allen Tasks     
-    
+
     //jetzt nach offenen sortieren
     this.filteredTasks = this.allTasks.filter((t) => t.status == "offen")   //nur offene Tasks anzeigen
-    .sort((a,b) => {
+      .sort((a, b) => {
         let dateA = new Date(a.date.split('.').reverse().join('-'));
         let dateB = new Date(b.date.split('.').reverse().join('-'));
         return dateA.getTime() - dateB.getTime();
       }); //sort mit KI erstellt
   }
-  
- delete(_id: string): void {    //deleteOne im backendService aufrufen (gibt message zurück)
+
+  delete(_id: string): void {    //deleteOne im backendService aufrufen (gibt message zurück)
     this.backendService.deleteOne(String(_id))
-    .then(() => { this.ngOnInit();       
-      this.deleteStatus = true;
-    });    
-  
-/**
-  delete(_id: string): void {    
-    this.backendService.getOne(String(_id)) //getOne im backendService aufrufen (gibt Promise Task zurück)
-    .then((response) => { 
-      this.task = response;  
-      this.deleteStatus = true;
-      console.log('delete status :' , this.deleteStatus)
-    });    
-  }  
+      .then(() => {
+        this.ngOnInit();
+        this.deleteStatus = true;
+      });
 
-  confirm() {
-    this.backendService.deleteOne(String(this.task._id))
-    .then( () => {
-      this.backendService.getAll()
-      .then( response => {
-        this.allTasks = response 
+    /**
+      delete(_id: string): void {    
+        this.backendService.getOne(String(_id)) //getOne im backendService aufrufen (gibt Promise Task zurück)
+        .then((response) => { 
+          this.task = response;  
+          this.deleteStatus = true;
+          console.log('delete status :' , this.deleteStatus)
+        });    
+      }  
+    
+      confirm() {
+        this.backendService.deleteOne(String(this.task._id))
+        .then( () => {
+          this.backendService.getAll()
+          .then( response => {
+            this.allTasks = response 
+            this.deleteStatus=false;
+          })
+        })
+      }
+      
+    
+      cancel(): void{
         this.deleteStatus=false;
-      })
-    })
+      }*/
   }
-  
 
-  cancel(): void{
-    this.deleteStatus=false;
-  }*/
+  markAsDone(_id: string): void {
+    //Task holen
+    //mit update auf erledigt ändern
+
+    this.backendService
+      .getOne(String(_id))      //task holen
+      .then((response) => {     //bekomme Promise Task zurück
+        this.task = response;
+        this.task.status = "erledigt";
+        return this.task;
+      })
+      .then(() => {
+        this.backendService.update(_id, this.task)
+        .then(() => {   // updateMethode des Service aufrufen (diese spricht wiederum update im backend an)
+            this.ngOnInit();    //refresh der Seite
+          })
+      })
+  
   }
 
 }
